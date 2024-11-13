e4c057fb33c8e63eda72ae79ba76ada0d8a9e2a0
Ira Zibbu <ira.zibbu@gmail.com>
2024-11-13 14:42:39 -0600

    write unit test for find_reindex_bases, and add rules to run tests to snakefile

--- test/scripts/find_reindex_bases_test.py
+++ test/scripts/find_reindex_bases_test.py
@@ -4,11 +4,36 @@ Test for find_reindex_bases.py
 """
 
 __author__ = "Ira Zibbu"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 ''' imports '''
-import os
 import argparse
-from re import sub
-from Bio.Seq import Seq
-from Bio import SeqIO
+
+parser = argparse.ArgumentParser(description='run test on output of find_reindex_bases rule')
+parser.add_argument('--file', help='txt file with bases output by find_reindex_bases.py')
+parser.add_argument('--output', help='output file to write to')
+
+def test(file):
+
+    with open(file, "r") as file_object:
+        for line in file_object:
+            bases=line.strip()
+
+        # These conditions are true from prior runs and is the expected result
+        if "yersinia" in file:
+            assert bases=="TCGCGCGATCTT",f"Reindex bases were not found in {file}"
+
+    flag=True
+    return flag # this is returned if all tests ran without an assertionerror
+
+def main(file,output):
+
+    flag = test(file)
+    if flag:
+        with open(output, "w") as file:
+            file.write("test successful")
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args.file,args.output)
