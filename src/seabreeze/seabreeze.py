79546dd5f430b70dd1a92aadab515b4d25c7eb2b
Ira Zibbu <ira.zibbu@gmail.com>
2025-05-15 10:53:32 -0500

    Update seabreeze CLI with run_all command

--- src/seabreeze/seabreeze.py
+++ src/seabreeze/seabreeze.py
@@ -1,7 +1,7 @@
 """
 seabreeze.py, a wrapper script around a snakemake pipeline to analyse structural variants in bacterial genomes
 Author: Ira Zibbu
-Version: 0.0.1
+Version: 0.0.3
 """
 
 """ imports """
@@ -15,6 +15,7 @@ import coloredlogs
 from seabreeze import __version__
 import shutil
 import csv
+import pandas as pd
 
 # set up logger
 coloredlogs.install(level='DEBUG')
@@ -37,6 +38,7 @@ def get_snakefile():
 @click.version_option(__version__)
 
 def cli():
+
     """
     seabreeze is a tool for comprehensively analyzing genetic variation among bacterial genomes caused by structural mutations.
     To get help, run `seabreeze --help`
@@ -61,7 +63,8 @@ def cli():
             "predict_structural_variants",
             "predict_replichore_balance",
             "predict_SV_mechanism",
-            "annotate_SV_regions"
+            "annotate_SV_regions",
+            "run_all"
         ]
     ),
 )
@@ -104,13 +107,13 @@ def run_smk_batch_workflow(workflow,dir,data,oridif,masked,threads,snakemake_arg
 
     """ Determine snakemake command to run based on supplied args """
 
-    logger.info(f"==================================")
-    logger.info(f"seabrezee version: {__version__}")
-    logger.info(f"==================================")
+    logger.debug(f"==================================")
+    logger.debug(f"seabrezee version: {__version__}")
+    logger.debug(f"==================================")
 
     # make the working directory if it does not already exist
     if not os.path.exists(dir):
-        logger.info(f"{dir} does not exist. Creating the directory..")
+        logger.debug(f"{dir} does not exist. Creating the directory..")
         try:
             os.makedirs(dir)
         except OSError as e:
@@ -122,6 +125,30 @@ def run_smk_batch_workflow(workflow,dir,data,oridif,masked,threads,snakemake_arg
         logger.critical(f"{data} does not exist. Aborting")
         exit(1)
 
+
+    df = pd.read_csv(data)
+
+    # Check if the csv has exactly two columns
+    if df.shape[1] != 2:
+        logger.critical("data.csv must have exactly two columns.")
+
+    # Check if the columns are named correctly
+    expected_columns = ['assembly', 'ancestor']
+    if list(df.columns) != expected_columns:
+        logger.critical(f"The data.csv columns must be named {expected_columns}. Found: {list(df.columns)}")
+
+    # Check if there are any duplicate entries for assemblies
+    if df['assembly'].duplicated().any():
+        logger.critical("There are duplicate entries in the assembly column.")
+
+    list_of_ancestors = df['ancestor'].tolist()
+    list_of_assemblies = df['assembly'].tolist()
+
+    for ancestor in list_of_ancestors:
+        if not(ancestor in list_of_assemblies):
+            logger.critical(f"Ancestor genome {ancestor} needs a self-to-self comparison. Please append this line to data.csv: {ancestor},{ancestor}")
+
+
     # ori and dif sequences are required to predict replichore balance:
     if workflow == "predict_replichore_balance":
         if not os.path.exists(oridif):
@@ -129,10 +156,10 @@ def run_smk_batch_workflow(workflow,dir,data,oridif,masked,threads,snakemake_arg
             exit(1)
 
     os.chdir(dir)
-    logger.info(f"Switch working directory to {os.getcwd()}")
-    logger.info(f"seabrezee version: {__version__}")
+    logger.debug(f"Switch working directory to {os.getcwd()}")
+    logger.debug(f"seabrezee version: {__version__}")
 
-    masked_workflows=["predict_structural_variants","predict_replichore_balance","predict_SV_mechanism","annotate_SV_regions"]
+    masked_workflows=["predict_structural_variants","predict_replichore_balance","predict_SV_mechanism","annotate_SV_regions","run_all"]
     if workflow in masked_workflows and masked:
         workflow=f"{workflow}_masked"
 
@@ -179,7 +206,8 @@ def run_smk_batch_workflow(workflow,dir,data,oridif,masked,threads,snakemake_arg
             "predict_structural_variants",
             "predict_replichore_balance",
             "predict_SV_mechanism",
-            "annotate_SV_regions"
+            "annotate_SV_regions",
+            "run_all"
         ]
     ),
 )
@@ -190,15 +218,6 @@ def run_smk_batch_workflow(workflow,dir,data,oridif,masked,threads,snakemake_arg
     help="Set working directory. Default is current working directory",
     default=".",
 )
-
-@click.option(
-    "-b",
-    "--data",
-    type=click.Path(dir_okay=True, writable=True, resolve_path=True),
-    help="Path to data.csv file. Default is data.csv in current working dir",
-    default="./data.csv",
-)
-
 
 @click.option(
     "--ancestor",
@@ -240,15 +259,15 @@ def run_smk_single_workflow(workflow,dir,assembly,ancestor,ori,dif,masked,thread
 
     """ Determine snakemake command to run based on supplied args """
 
-    logger.info(f"==================================")
-    logger.info(f"seabrezee version: {__version__}")
-    logger.info(f"==================================")
+    logger.debug(f"==================================")
+    logger.debug(f"seabrezee version: {__version__}")
+    logger.debug(f"==================================")
 
 
     # check if working dir exists. If not, create.
 
     if not os.path.exists(dir):
-        logger.info(f"{dir} does not exist. Creating the directory..")
+        logger.debug(f"{dir} does not exist. Creating the directory..")
         try:
             os.makedirs(dir)
         except OSError as e:
@@ -265,17 +284,22 @@ def run_smk_single_workflow(workflow,dir,assembly,ancestor,ori,dif,masked,thread
     dir_for_fasta_files = os.path.join(dir, "02_genomes")
 
     # make the dir for the fasta files and copy them over
-    try:
-        os.makedirs(dir_for_fasta_files)
-    except OSError as e:
-        logger.critical(f"Unable to create {dir_for_fasta_files}")
-        logger.critical(e)
-        exit(1)
+
+    if os.path.exists(dir_for_fasta_files):
+        logger.debug(f"{dir_for_fasta_files} exists. No requirement to make file")
+
+    if not os.path.exists(dir_for_fasta_files):
+        try:
+            os.makedirs(dir_for_fasta_files)
+        except OSError as e:
+            logger.critical(f"Unable to create {dir_for_fasta_files}")
+            logger.critical(e)
+            exit(1)
 
     try:
         for file_path in [ancestor, assembly]:
             shutil.copy(file_path, dir_for_fasta_files)
-            logger.info(f"Copied {file_path} to {dir_for_fasta_files}")
+            logger.debug(f"Copied {file_path} to {dir_for_fasta_files}")
     except Exception as e:
         logger.critical(f"Error copying files: {e}")
         exit(1)
@@ -283,14 +307,14 @@ def run_smk_single_workflow(workflow,dir,assembly,ancestor,ori,dif,masked,thread
     # define the data.csv from scratch
 
     os.chdir(dir)
-    logger.info(f"Switch working directory to {os.getcwd()}")
+    logger.debug(f"Switch working directory to {os.getcwd()}")
 
     ancestor_name = os.path.splitext(os.path.basename(ancestor))[0]
     assembly_name = os.path.splitext(os.path.basename(assembly))[0]
 
     data_csv_table = [
-        {'ancestor': ancestor_name, 'assembly': assembly_name},
-        {'ancestor': ancestor_name, 'assembly': ancestor_name}
+        {'assembly': assembly_name, 'ancestor': ancestor_name},
+        {'assembly': ancestor_name, 'ancestor': ancestor_name}
     ]
 
     csv_file = "data.csv"
@@ -299,34 +323,33 @@ def run_smk_single_workflow(workflow,dir,assembly,ancestor,ori,dif,masked,thread
             writer = csv.DictWriter(file, fieldnames=['ancestor', 'assembly'])
             writer.writeheader()
             writer.writerows(data_csv_table)
-        logger.info(f"data.csv written successfully to {csv_file}")
+        logger.debug(f"data.csv written successfully to {csv_file}")
     except Exception as e:
         logger.critical(f"Error writing CSV: {e}")
 
     # ori and dif sequences are required to predict replichore balance:
-    if workflow == "predict_replichore_balance":
+    if workflow == "predict_replichore_balance" or workflow == "run_all":
         if not ori or not dif:
-            logger.critical("Both --ori and --dif options must be supplied for the predict_replichore_balance workflow.")
+            logger.critical("Both --ori and --dif options must be supplied for the predict_replichore_balance or run_all workflow.")
             exit(1)
 
         # write out an ori_dif_sequences.csv file
 
-        ori_dif_table = [
-            {'ancestor': ancestor_name, 'ori': ori, 'dif': dif}]
+    ori_dif_table = [
+        {'ancestor': ancestor_name, 'ori': ori, 'dif': dif}]
 
-        csv_file = "ori_dif_sequences.csv"
-        try:
-            with open(csv_file, mode='w', newline='') as file:
-                writer = csv.DictWriter(file, fieldnames=['ancestor', 'ori', 'dif'])
-                writer.writeheader()
-                writer.writerows(ori_dif_table)
-            logger.info(f"ori_dif_sequences.csv written successfully to {csv_file}")
-        except Exception as e:
-            logger.critical(f"Error writing CSV: {e}")
-            exit(1)
+    csv_file = "ori_dif_sequences.csv"
+    try:
+        with open(csv_file, mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['ancestor', 'ori', 'dif'])
+            writer.writeheader()
+            writer.writerows(ori_dif_table)
+        logger.debug(f"ori_dif_sequences.csv written successfully to {csv_file}")
+    except Exception as e:
+        logger.critical(f"Error writing CSV: {e}")
+        exit(1)
 
-
-    masked_workflows=["predict_structural_variants","predict_replichore_balance","predict_SV_mechanism","annotate_SV_regions"]
+    masked_workflows=["predict_structural_variants","predict_replichore_balance","predict_SV_mechanism","annotate_SV_regions","run_all"]
     if workflow in masked_workflows and masked:
         workflow=f"{workflow}_masked"
 
